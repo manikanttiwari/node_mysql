b069e7f2bb7be4bd211396715c344fe3003e41f8
manikanttiwari <32715824+manikanttiwari@users.noreply.github.com>
2020-10-31 13:30:48 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'manikant@@123',
+    password: //your database password,
     database: 'nodemysql'
 });
 
